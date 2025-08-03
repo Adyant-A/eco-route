0761cd14cedca744dd0c71ecafaf581495dec9f4
Adyant <inadyantbox@gmail.com>
2025-08-03 15:43:23 -0700

    homepage

--- eco-route/src/app/page.js
+++ eco-route/src/app/page.js
@@ -1,15 +1,21 @@
 import styles from "./page.module.css";
-import {Button} from '@heroui/button'; 
+import Image from 'next/image';
 
+import homepage from '../app/images/homepage.png';
+
+import { Button } from '@heroui/button';
+import {Input} from "@heroui/react";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <p>Testing</p>
-       <Button>Click me</Button>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className={styles.page}>  
+      <div className={styles.container}>
+        <Image src={homepage} className={styles.image} alt="homepage" style={{marginTop: "60px", marginBottom: "30px"}}/>
+        {/* TODO: add username, password, fix padding, submit github */}
+        <Input label="Username" placeholder="Enter your username" type="username" className={styles.input} key={"primary"} color={"primary"}/>
+        <Input label="Password" placeholder="Enter your password" type="password" className={styles.input} key={"primary"} color={"primary"}/>
+        <Button className={styles.button}>LOGIN</Button>
+      </div>
     </div>
   );
 }
