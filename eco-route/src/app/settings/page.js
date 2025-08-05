f4cd7f7fdac4dec0fc64ca10af986f149ee03050
Adyant <inadyantbox@gmail.com>
2025-08-04 20:07:25 -0700

    settings page completed

--- eco-route/src/app/settings/page.js
+++ eco-route/src/app/settings/page.js
@@ -26,13 +26,30 @@ export default function Settings() {
           alt="User Avatar" 
           style={{marginBottom: "20px"}}
         />
+         <h3>My Profile</h3>
          <Input label="Name" placeholder="Adyant Akula" className={styles.input} key={"primary"}/>
          <Input label="Home Address" placeholder="1234 Crescent Avenue, San Ramon, 94582" className={styles.input} key={"primary"}/>
          <Input label="Phone Number" placeholder="925-123-4567" className={styles.input} key={"primary"}/>
-         <Switch defaultSelected>Enable text messages</Switch>
-         <Switch defaultSelected>Car preferred</Switch>
-         <Switch defaultSelected>Enable text messages</Switch>
-         <p>Member since August 2025</p>
+         <div className={styles.switchContainer}>
+           <Switch size="sm" color="success" defaultSelected><p style={{fontSize:"14px"}}>Enable text messages</p></Switch>
+         </div>
+          <div className={styles.switchContainer}>
+           <Switch size="sm" color="success" defaultSelected><p style={{fontSize:"14px"}}>Enable notifications</p></Switch>
+         </div>
+         <h3>Transportation Preferences</h3>
+         <div className={styles.switchContainer}>
+           <Switch size="sm" color="success"><p style={{fontSize:"14px"}}>I have a car</p></Switch>
+         </div>
+         <div className={styles.switchContainer}> 
+           <Switch size="sm" defaultSelected color="success"><p style={{fontSize:"14px"}}>I have a bicycle</p></Switch>
+         </div>
+         <div className={styles.switchContainer}> 
+           <Switch size="sm" defaultSelected color="success"><p style={{fontSize:"14px"}}>I use transit (BART, AC)</p></Switch>
+         </div>
+         <div className={styles.switchContainer}> 
+           <Switch size="sm" color="success"><p style={{fontSize:"14px"}}>I use Uber</p></Switch>
+         </div>
+         <p style={{fontWeight:"bold", color:"gray"}}>Member since August 2025</p>
         <BottomBar activeTab="settings" />
       </div>
     </div>
